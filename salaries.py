81b61322fdbe5a72b8237128ef9da2549ae3649d
Leonardo de la Cerda <leonardo@agavelab.com>
2019-04-06 21:08:56 -0700

    Business logic class Salaries that will process the salaries

---
+++ salaries.py
@@ -0,0 +1,92 @@
+from decimal import Decimal
+
+class ResuelveFCError(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class LevelNotFoundException(ResuelveFCError):
+    pass
+
+class Salaries(object):
+    goals_per_level_dict = {'A': 5, 'B': 10, 'C': 15, 'Cuauh': 20}
+    TWOPLACES = Decimal(10) ** -2
+
+    def __init__(self, all_player_stats):
+        self.player_stats = all_player_stats
+        self.team_goal = dict()
+        self.team_scored = dict()
+
+
+    def process(self):
+        self.resolve_players_level()
+        self.grouping_teams()
+
+        for player in self.player_stats:
+            player['sueldo_completo'] = self.get_player_salary(player)
+
+        return self.player_stats
+
+    def resolve_players_level(self):
+        for player in self.player_stats:
+            player_level = player['nivel']
+
+            player_goal = self.goals_per_level(player_level)
+            player['goles_minimos'] = player_goal
+
+
+    def grouping_teams(self):
+        for player in self.player_stats:
+            player_team = player['equipo']
+            player_goal = player['goles_minimos']
+            player_scored = player['goles']
+
+            # Scored goals per team
+            current_team_scored = self.team_scored.get(player_team, 0)
+            new_team_scored = current_team_scored + player_scored
+            self.team_scored[player_team] = new_team_scored
+
+            # Team required goals to achieve
+            current_team_goal = self.team_goal.get(player_team, 0)
+            new_team_goal = player_goal + current_team_goal
+            self.team_goal[player_team] = new_team_goal
+
+
+    def get_player_salary(self, player):
+        player_fixed = Decimal(player['sueldo'])
+        player_bonus = Decimal(player['bono'])
+        player_team = player['equipo']
+
+        player_rate = self.player_rating(player)
+        team_rate = self.team_rating(player_team)
+
+        # 50% personal rate + 50% team rate
+        final_bonus = player_bonus * (player_rate + team_rate) / 2
+        final_salary = player_fixed + final_bonus.quantize(Salaries.TWOPLACES)
+        return final_salary
+
+
+    def team_rating(self, team_name):
+        one = Decimal(1)
+        rate = Decimal(self.team_scored[team_name]) \
+               / Decimal(self.team_goal[team_name])
+        if( rate > one ):
+            return one
+        else:
+            return rate
+
+
+    def player_rating(self, player):
+        one = Decimal(1)
+        rate = Decimal(player['goles']) \
+               / Decimal(player['goles_minimos'])
+        if( rate > one ):
+            return one
+        else:
+            return rate
+
+
+    def goals_per_level(self, level):
+        if not level in self.goals_per_level_dict:
+            raise LevelNotFoundException(level + ' level does not exist')
+
+        return self.goals_per_level_dict[level]
---
+++ tests/salaries_test.py
@@ -0,0 +1,147 @@
+import unittest
+from salaries import *
+from nose.tools import *
+from decimal import Decimal
+
+class SalariesTest(unittest.TestCase):
+
+    def setUp(self):
+        player1 =    {
+            "nombre": "Juan Perez",
+            "nivel": "C",
+            "goles": 10,
+            "sueldo": 50000,
+            "bono": 25000,
+            "sueldo_completo": None,
+            "equipo": "rojo"
+        }
+        player2 = {
+            "nombre": "EL Cuauh",
+            "nivel": "Cuauh",
+            "goles": 30,
+            "sueldo": 100000,
+            "bono": 30000,
+            "sueldo_completo": None,
+            "equipo": "azul"
+        }
+        player3 = {
+            "nombre": "Cosme Fulanito",
+            "nivel": "A",
+            "goles": 7,
+            "sueldo": 20000,
+            "bono": 10000,
+            "sueldo_completo": None,
+            "equipo": "azul"
+        }
+        player4 =    {
+            "nombre": "El Rulo",
+            "nivel": "B",
+            "goles": 9,
+            "sueldo": 30000,
+            "bono": 15000,
+            "sueldo_completo": None,
+            "equipo": "rojo"
+        }
+        self.original_input = [ player1,
+                                player2,
+                                player3,
+                                player4 ]
+        self.salaries = Salaries(self.original_input)
+
+    def test_exceeded_team_rate(self):
+        """Assert that the team rate cannot go above 1"""
+        self.salaries.team_scored['chivas'] = 20
+        self.salaries.team_goal['chivas'] = 10
+        rate = self.salaries.team_rating('chivas')
+        self.assertEqual(Decimal(1), rate)
+
+    def test_team_rate(self):
+        """Assert a correct team rate calculation"""
+        self.salaries.team_scored['chivas'] = 10
+        self.salaries.team_goal['chivas'] = 20
+        rate = self.salaries.team_rating('chivas')
+        self.assertEqual(Decimal(0.5), rate)
+
+    def test_exceeded_player_rate(self):
+        """Assert that the player rate cannot go above 1"""
+        player = {
+                'goles': 100,
+                'goles_minimos': 10
+                }
+        rate = self.salaries.player_rating(player)
+        self.assertEqual(Decimal(1), rate)
+
+    def test_player_rate(self):
+        """Assert a correct player rate calculus"""
+        player = {
+                'goles': 10,
+                'goles_minimos': 100
+                }
+        rate = self.salaries.player_rating(player)
+        self.assertEqual(Decimal('0.1'), rate)
+
+    @raises(LevelNotFoundException)
+    def test_a_non_existing_level(self):
+        """Assert that we raise when we find a non existing level"""
+        player = {
+            "nombre": "El Rulo",
+            "nivel": "non_existing_level",
+            "goles": 9,
+            "sueldo": 30000,
+            "bono": 15000,
+            "sueldo_completo": None,
+            "equipo": "rojo"
+        }
+        salary = Salaries([player])
+        salary.process()
+
+
+    def test_player_levels(self):
+        """Assert that we process correctly the goals per level"""
+        result = self.salaries.process()
+        self.assertEqual(result[0]['goles_minimos'], 15)
+        self.assertEqual(result[1]['goles_minimos'], 20)
+        self.assertEqual(result[2]['goles_minimos'], 5)
+        self.assertEqual(result[3]['goles_minimos'], 10)
+
+
+    def test_teams_count(self):
+        """Assert that we count correctly the number of teams"""
+        result = self.salaries.process()
+        self.assertEqual(len(self.salaries.team_goal), 2)
+        self.assertEqual(len(self.salaries.team_scored), 2)
+
+    def test_teams_processing(self):
+        """Assert that we process correctly the team grouping"""
+        result = self.salaries.process()
+        self.assertEqual(self.salaries.team_goal['azul'], 25)
+        self.assertEqual(self.salaries.team_goal['rojo'], 25)
+        self.assertEqual(self.salaries.team_scored['azul'], 37)
+        self.assertEqual(self.salaries.team_scored['rojo'], 19)
+
+    def test_stuff_that_should_remain(self):
+        """Assert that we are not modifying anything that we shouldn't"""
+        result_list = self.salaries.process()
+        key_list = ['nombre', 'nivel', 'goles', 'sueldo', 'bono', 'equipo']
+
+        for origin, result in zip(self.original_input, result_list):
+            for key in key_list:
+                self.assertEqual(origin[key], result[key])
+
+    def test_correct_final_result(self):
+        """Assert that we are processing correctly a final salary"""
+        result_list = self.salaries.process()
+
+        self.assertEqual(result_list[0]['sueldo_completo'],
+                         Decimal('67833.33'))
+        self.assertEqual(result_list[1]['sueldo_completo'],
+                         Decimal('130000.00'))
+        self.assertEqual(result_list[2]['sueldo_completo'],
+                         Decimal('30000.00'))
+        self.assertEqual(result_list[3]['sueldo_completo'],
+                         Decimal('42450.00'))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
